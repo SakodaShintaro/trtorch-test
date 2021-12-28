31cebb862dbd65041d252262edfc9091fba21956
sakoda <sakoda@keio.jp>
2021-12-28 15:31:29 +0900

    use TransformerEncoderLayer

--- model.py
+++ model.py
@@ -2,19 +2,7 @@
 import torch.jit
 import torch.nn as nn
 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.convs = nn.ModuleList([nn.Conv2d(256, 256, 3, padding=1) for _ in range(100)])
-
-    def forward(self, x):
-        for c in self.convs:
-            x = c.forward(x)
-        return x
-
-
-model = Net()
-input_data = torch.empty([1, 256, 32, 32])
+model = nn.TransformerEncoderLayer(256, 8)
+input_data = torch.empty([1, 32, 256])
 traced_model = torch.jit.trace(model, input_data)
 traced_model.save("model.ts")

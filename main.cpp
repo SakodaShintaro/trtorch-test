dfd147b73dc2b531ffb0b7441809a9b4d1dab5d3
sakoda <sakoda@keio.jp>
2021-12-28 07:03:04 +0000

    refactoring

--- main.cpp
+++ main.cpp
@@ -3,43 +3,26 @@
 using namespace std;
 
 void compile(bool fp16) {
+  auto type = (fp16 ? torch::kF16 : torch::kF32);
   torch::jit::Module module = torch::jit::load("model.ts");
-  if (fp16) {
-    module.to(torch::kCUDA, torch::kHalf);
-  } else {
-    module.to(torch::kCUDA);
-  }
+  module.to(torch::kCUDA, type);
   module.eval();
 
   constexpr int64_t INPUT_CHANNEL_NUM = 256;
   constexpr int64_t WIDTH = 32;
   std::vector<int64_t> in_sizes = {1, WIDTH, INPUT_CHANNEL_NUM};
-  torch_tensorrt::Input range(in_sizes);
+  torch_tensorrt::Input range(in_sizes, type);
   torch_tensorrt::ts::CompileSpec info({range});
-  if (fp16) {
-    info.enabled_precisions.insert(torch::kHalf);
-  } else {
-    info.enabled_precisions.insert(torch::kF32);
-  }
+  info.enabled_precisions.insert(type);
   module = torch_tensorrt::ts::compile(module, info);
 }
 
 int main() {
-  // fp32, this thread -> OK
+  // fp32, this thread -> NG
   compile(false);
   cout << "fp32, this thread -> finish" << endl;
 
-  // fp32, another thread -> OK
-  std::thread thread0([]() { compile(false); });
-  thread0.join();
-  cout << "fp32, another thread -> finish" << endl;
-
-  // fp16, this thread -> OK
+  // fp16, this thread -> NG
   compile(true);
   cout << "fp16, this thread -> finish" << endl;
-
-  // fp16, another thread -> NG
-  std::thread thread1([]() { compile(true); });
-  thread1.join();
-  cout << "fp16, another thread -> finish" << endl;
 }

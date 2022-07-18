c980cb7010b44bf84ef90689e60abc9a5fc6b94c
Shintaro Sakoda <sakoda@keio.jp>
2022-07-18 17:09:04 +0900

    Impl shogi model

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 void compile(bool fp16) {
-  constexpr int64_t INPUT_CHANNEL_NUM = 256;
-  constexpr int64_t WIDTH = 32;
-  torch::jit::Module module = torch::jit::load("model.ts");
+  constexpr int64_t INPUT_CHANNEL_NUM = 42;
+  constexpr int64_t WIDTH = 9;
+  torch::jit::Module module = torch::jit::load("shogi_cat_bl10_ch256.ts");
   if (fp16) {
-    module.to(torch::kCUDA, torch::kHalf);
+    module.to(torch::kCUDA, torch::kFloat16);
   } else {
     module.to(torch::kCUDA);
   }
@@ -15,13 +15,24 @@ void compile(bool fp16) {
 
   std::vector<int64_t> in_sizes = {1, INPUT_CHANNEL_NUM, WIDTH, WIDTH};
 
-  auto input = (fp16 ? torch_tensorrt::Input(in_sizes, torch::kHalf) : torch_tensorrt::Input(in_sizes, torch::kFloat32));
+  auto input =
+      (fp16 ? torch_tensorrt::Input(in_sizes, torch::kFloat16) : torch_tensorrt::Input(in_sizes, torch::kFloat32));
   auto compile_settings = torch_tensorrt::ts::CompileSpec({input});
   if (fp16) {
-    compile_settings.enabled_precisions = {torch::kHalf};
-  } else {
+    compile_settings.enabled_precisions = {torch::kFloat16};
   }
   module = torch_tensorrt::ts::compile(module, compile_settings);
+
+  torch::Tensor sample_input = torch::zeros({1, INPUT_CHANNEL_NUM, WIDTH, WIDTH}).to(torch::kCUDA);
+  if (fp16) {
+    sample_input = sample_input.to(torch::kFloat16);
+  }
+
+  auto out = module.forward({sample_input});
+  auto tuple = out.toTuple();
+  torch::Tensor policy = tuple->elements()[0].toTensor();
+  torch::Tensor value = tuple->elements()[1].toTensor();
+  std::cout << policy.sizes() << " " << value.sizes() << std::endl;
 }
 
 int main() {
